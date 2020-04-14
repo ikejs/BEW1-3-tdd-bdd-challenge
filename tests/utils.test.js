ff8c3eab7d83313ad51c6158c1d1b88eee6182af
Ike <ikescode@gmail.com>
2020-04-14 13:54:32 -0500

    all passing

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,7 +1,7 @@
-const mocha = require("mocha")
-const chai = require("chai")
-const utils = require("../utils")
-const expect = chai.expect
+const mocha = require("mocha");
+const chai = require("chai");
+const utils = require("../utils");
+const expect = chai.expect;
 
 // ========================================================
 // NOTE: https://mochajs.org/#arrow-functions
@@ -10,21 +10,34 @@ const expect = chai.expect
 // ========================================================
 
 it("should say hello", function() {
-  const hello = utils.sayHello()
-  expect(hello).to.be.a("string")
-  expect(hello).to.equal("Hello")
-  expect(hello).with.lengthOf(5)
-})
+  const hello = utils.sayHello();
+  expect(hello).to.be.a("string");
+  expect(hello).to.equal("Hello");
+  expect(hello).with.lengthOf(5);
+});
 
 // ========================================================
 // Level 1 Challenges
 // ========================================================
 
-it("should return the area of a 5 by 6 rectangle")
+it("should return the area of a 5 by 6 rectangle", function() {
+  const area = utils.area(5, 6);
+  expect(area).to.be.a("number");
+  expect(area).to.equal(30);
+});
 
-it("should return the perimeter of a 5 by 6 rectangle")
+it("should return the perimeter of a 5 by 6 rectangle", function() {
+  const perimeter = utils.perimeter(5, 6);
+  expect(perimeter).to.be.a("number");
+  expect(perimeter).to.equal(22)
+  
+});
 
-it("should return the are of a circle of radius 5")
+it("should return the are of a circle of radius 5", function() {
+  const area = utils.circleArea(5);
+  expect(area).to.be.a("number");
+  expect(area).to.equal(78.53981633974483);
+});
 
 // ========================================================
 // Level 2 Challenges
@@ -34,22 +47,41 @@ it("should return the are of a circle of radius 5")
 // tests eventually.
 // ========================================================
 
-it("Should create a new (object) Item with name and price")
+it("Should create a new (object) Item with name and price", function() {
+  const item = utils.createItem("apple", 0.99, 1);
+  expect(item).to.be.an("object");
+  expect(item).to.have.property("name", "apple");
+  expect(item).to.have.property("price", 0.99);
+  expect(item).to.have.property("quantity", 1);
+});
 
-it("Should return an array containing all items in cart")
+it("Should return an array containing all items in cart", function() {
+  const cart = utils.showCart();
+  expect(cart).to.be.an("array");
+});
 
-it("Should add a new item to the shopping cart")
+it("Should add a new item to the shopping cart", function() {
+  const addToCart = utils.addToCart({ name: "orange", price: 1.20, quantity: 2 });
+  expect(addToCart).to.be.an("array");
+});
 
-it("Should return the number of items in the cart")
+it("Should return the number of items in the cart", function() {
+  const totalCartItems = utils.totalCartItems();
+  expect(totalCartItems).to.be.a("number");
+  expect(totalCartItems).to.equal(2);
+});
 
-it("Should remove items from cart")
+it("Should remove items from cart", function() {
+  const removeItemFromCart = utils.removeItemFromCart({ name: "orange" });
+  expect(removeItemFromCart).to.be.an("array");
+});
 
 // ========================================================
 // Stretch Challenges
 // ========================================================
 
-it("Should update the count of items in the cart")
+it("Should update the count of items in the cart");
 
-it("Should validate that an empty cart has 0 items")
+it("Should validate that an empty cart has 0 items");
 
-it("Should return the total cost of all items in the cart")
+it("Should return the total cost of all items in the cart");
--- utils.js
+++ utils.js
@@ -25,9 +25,40 @@ const circleArea = r => {
 // defined) in order to make the tests pass.
 // ========================================================
 
+cart = [];
 
+const createItem = (name, price, quantity) => {
+  return { name, price, quantity }
+}
+
+const addToCart = (item) => {
+  cart.push(item);
+  return cart;
+}
+
+const showCart = () => {
+  return cart;
+}
+
+const totalCartItems = () => {
+  let quantity = 0
+  for(i=0; i < cart.length; i+=1) {
+    quantity += cart[i].quantity;
+  }
+  return quantity;
+}
+
+const removeItemFromCart = (itemToRemove) => {
+  for (i=0; i < cart.length; i += 1) {
+    if (cart[i].name === itemToRemove.name) {
+      cart.slice(i, 1);
+    }
+  }
+  return cart;
+}
 
 
 module.exports = { 
-  sayHello, area, perimeter, circleArea
+  sayHello, area, perimeter, circleArea,
+  createItem, addToCart, showCart, totalCartItems, removeItemFromCart
 }
